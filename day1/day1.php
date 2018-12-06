10a60ca21cfa0cf114e5ddd6d4d86645279427f5
Marcus Carlsson <marcus.carlsson@barnebys.com>
2018-12-06 17:07:47 +0100

    Clean it up

--- day1/day1.php
+++ day1/day1.php
@@ -1,19 +1,16 @@
 <?php
-
+// Get the input numbers
 $inputs = file_get_contents('./inputs.txt');
+// Move it into an array
 $inputsArray = explode(PHP_EOL, $inputs);
-
+// Puzzle was to start at zero
 $frequency = 0;
 
-echo '-------------------------';
-
-echo gettype($inputsArray);
-
-
+// Loop trough the array
 foreach ($inputsArray as $input) {
+    // add each input number to the frequency variable
     $frequency += $input;
 }
-
-
+//Dump it out
 echo 'Answer is: ' . $frequency . ' is the new frequency';
 
